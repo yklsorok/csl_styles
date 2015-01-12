900ed8cb20f5433af5f6cccaa1fc475402ab61a7
Anton Iershov <yklsorok@gmail.com>
2015-01-12 13:49:50 +0200

    linux script updated, pages converted to long format

--- update-database-linux.py
+++ update-database-linux.py
@@ -24,17 +24,22 @@ def find(pattern, path):
 # where USER is the name of your user, email@server.com is your email
 # under Linux, it is usually like
 # "/home/USER/.local/share/data/Mendeley Ltd./Mendeley Desktop/email@server.com@www.mendeley.com.sqlite"
-# where USER is the name of your user, email@server.com is your email. 
-# !!!note, there should be the full path, otherwise sqlite3 will throw an error
+# where USER is the name of your user, email@server.com is your email.
+# !!!note, the path should be full, otherwise sqlite3 will throw an error
 
+# setup connection
 db_path = "/home/" + getpass.getuser() + "/.local/share/data/Mendeley Ltd./Mendeley Desktop/"
 db_filename = find('*mendeley.com.sqlite', db_path)
 conn = sqlite3.connect(db_filename[1])
+c = conn.cursor()
 
+# initialize variables
 result = {}
 doctype = {}
 fields = {}
-c = conn.cursor()
+pages = {}
+page_update_count = 0
+
 # get max document id
 c.execute('SELECT MAX(documentId) FROM DocumentContributors')
 maxId = c.fetchone()
@@ -42,9 +47,8 @@ maxId = c.fetchone()
 for i in range(maxId[0]):
     for row in c.execute('SELECT count(id) FROM documentContributors WHERE documentId=' + str(i)):
         result[i] = row[0]
-    for row2 in c.execute('SELECT type FROM documents WHERE id=' + str(i)):
+    for row2 in c.execute('SELECT type FROM Documents WHERE id=' + str(i)):
         doctype[i] = row2[0]
-#        print doctype[i]
 # create a dictionary where documentId is associated with required field
 
 for key in result.keys():
@@ -60,12 +64,46 @@ for key in result.keys():
             fields[key] = "city"
         elif a == 3:
             fields[key] = "genre"
-# update table "documents"
+
+# update pages fields if they are in short format
+for row in c.execute('SELECT id, pages FROM Documents'):
+    try:
+        pages_list = row[1].split("-")
+        # if two pages given
+        if len(pages_list) == 2:
+            first_page = pages_list[0]
+            last_page = pages_list[1]
+            # if page format is short
+            if int(last_page) < int(first_page):
+                last_page_upd = first_page[:-len(last_page)] + last_page
+                pages[row[0]] = first_page + "-" + last_page_upd
+                page_update_count += 1
+    except:
+        # if smth went wrong, i.e. pages are in P111 format
+        pass
+print(pages)
+# drop publisher/city/genre fields
+c.execute('UPDATE Documents SET publisher=NULL, city=NULL, genre=NULL WHERE type=\'JournalArticle\'')
+# save (commit) the changes
+conn.commit()
+
+# update table Documents by setting publisher/city/genre fields
 for key in fields.keys():
     b = fields[key]
     c.execute('UPDATE Documents SET ' + b + '=\'' + b + '\' WHERE id=' + str(key))
 # save (commit) the changes
 conn.commit()
+
+# update pages field in table Documents
+for key in pages.keys():
+    p = pages[key]
+    c.execute('UPDATE Documents SET pages=\'' + p + '\' WHERE id=' + str(key))
+# save (commit) the changes
+conn.commit()
+
+print(str(page_update_count) + " page fields updated")
+print("Publisher/City/Genre fields in " + str(len(fields.keys())) + " article records updated")
+input("Press any key to exit...")
 
 # close connection
 conn.close()
